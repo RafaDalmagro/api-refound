2494e0f4e142b7fc878daf1551707229f73a4f81
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-06 10:20:16 -0300

    Finalizando controller de session, implementando rotas e config de autenticação.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export const authConfig = {
+	jwt: {
+		secret: "rafael",
+		expiresIn: 86400, // 1 dia
+	},
+};
--- src/controllers/SessionsController.ts
+++ src/controllers/SessionsController.ts
@@ -1,8 +1,46 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { prisma } from "@/databases/prisma";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { authConfig } from "@/config/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
     async create(req: Request, res: Response, next: NextFunction) {
-        return res.status(201).json({ message: "Create" });
+        const bodySchema = z.object({
+            email: z.string().email({ message: "E-mail inválido" }),
+            password: z.string(),
+        });
+
+        const { email, password } = bodySchema.parse(req.body);
+
+        const user = await prisma.user.findFirst({
+            where: {
+                email,
+            },
+        });
+
+        if (!user) {
+            throw new AppError("E-mail ou senha inválidos", 401);
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new AppError("E-mail ou senha inválidos", 401);
+        }
+
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({ role: user.role }, secret, {
+            subject: user.id,
+            expiresIn,
+        });
+
+        const { password: _, ...userWithoutPassword } = user;
+
+        return res.status(201).json({ token, user: userWithoutPassword });
     }
 }
 
