ec13df4b60f85c47680a370ecf9a65ffdbb72d88
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-07 16:29:44 -0300

    Validando os dados dos reembolsos.

--- src/controllers/RefoundsController.ts
+++ src/controllers/RefoundsController.ts
@@ -1,10 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+
+const CategoriesEnum = z.enum(["food", "transport", "others", "accommodation"]);
 
 class RefoundsController {
-	async create(req: Request, res: Response, next: NextFunction) {
-		return res.status(201).json({ message: "Create" });
-	}
+    async create(req: Request, res: Response, next: NextFunction) {
+        const bodySchema = z.object({
+            name: z
+                .string()
+                .trim()
+                .min(2, { message: "Informe o nome da solicitação." }),
+            category: CategoriesEnum,
+            amount: z
+                .number()
+                .positive({ message: "O valor deve ser positivo." }),
+            filename: z.string().min(20),
+        });
 
+        const { name, category, amount, filename } = bodySchema.parse(req.body);
+
+        return res.status(201).json({ message: "Create" });
+    }
 }
 
 export { RefoundsController };
